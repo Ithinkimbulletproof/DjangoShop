b0cf6edd8eb74ef0fb95f3218f0cdc13ccc52e57
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-07-08 21:01:27 +0700

    Some fix

--- catalog/models.py
+++ catalog/models.py
@@ -18,7 +18,8 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='product_images/')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100,
+                                default='1')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
