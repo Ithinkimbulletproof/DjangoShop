d854293bff9436ef33ce141479686ec04597443a
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-07-08 16:33:21 +0700

    Some fix

--- catalog/views.py
+++ catalog/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.views.generic.detail import DetailView
 from django.utils.text import slugify
-from django.urls import reverse
-from catalog.models import BlogPost
-from catalog.forms import BlogPostForm
+from .models import BlogPost
+from .forms import BlogPostForm
+
 
 class BlogPostListView(View):
     template_name = 'blog_post_list.html'
 
     def get(self, request):
         blog_posts = BlogPost.objects.filter(is_published=True)
-        return render(request, self.template_name, {'blog_posts': blog_posts})
+        return render(request, self.template_name,
+                      {'blog_posts': blog_posts})
+
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
@@ -26,6 +28,7 @@ class BlogPostDetailView(DetailView):
         obj.save()
         return obj
 
+
 class BlogPostCreateView(View):
     template_name = 'blog_post_form.html'
 
@@ -36,12 +39,12 @@ class BlogPostCreateView(View):
     def post(self, request):
         form = BlogPostForm(request.POST, request.FILES)
         if form.is_valid():
-            # Сохранение формы и генерация slug
             blog_post = form.save(commit=False)
             blog_post.slug = slugify(blog_post.title)
             blog_post.save()
             return redirect('blog_post_list')
         return render(request, self.template_name, {'form': form})
+
 
 class BlogPostUpdateView(View):
     template_name = 'blog_post_form.html'
@@ -53,18 +56,21 @@ class BlogPostUpdateView(View):
 
     def post(self, request, pk):
         blog_post = get_object_or_404(BlogPost, pk=pk)
-        form = BlogPostForm(request.POST, request.FILES, instance=blog_post)
+        form = BlogPostForm(request.POST, request.FILES,
+                            instance=blog_post)
         if form.is_valid():
             form.save()
             return redirect('blog_post_detail', slug=blog_post.slug)
         return render(request, self.template_name, {'form': form})
+
 
 class BlogPostDeleteView(View):
     template_name = 'blog_post_confirm_delete.html'
 
     def get(self, request, pk):
         blog_post = get_object_or_404(BlogPost, pk=pk)
-        return render(request, self.template_name, {'blog_post': blog_post})
+        return render(request, self.template_name,
+                      {'blog_post': blog_post})
 
     def post(self, request, pk):
         blog_post = get_object_or_404(BlogPost, pk=pk)
--- djangoshop/urls.py
+++ djangoshop/urls.py
@@ -6,14 +6,5 @@ from catalog.views import BlogPostListView, BlogPostDetailView, BlogPostCreateVi
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('blog/', BlogPostListView.as_view(), name='blog_post_list'),
-    path('blog/', BlogPostListView.as_view(), name='blog_post_list'),
-    path('blog/<slug:slug>/', BlogPostDetailView.as_view(),
-         name='blog_post_detail'),
-    path('blog/new/', BlogPostCreateView.as_view(),
-         name='blog_post_create'),
-    path('blog/<int:pk>/edit/', BlogPostUpdateView.as_view(),
-         name='blog_post_update'),
-    path('blog/<int:pk>/delete/', BlogPostDeleteView.as_view(),
-         name='blog_post_delete'),
+    path('', BlogPostListView.as_view(), name='home')
 ]
