df1141981a81aaf373b3af1ff8e6da3112a8eef5
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-07-09 15:12:32 +0700

    Some fix

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from catalog.models import BlogPost
+from catalog.models import BlogPost, Product
 
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ['title', 'slug', 'content', 'preview_image', 'is_published']
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'image', 'category', 'price', 'manufactured_at']
+
--- catalog/models.py
+++ catalog/models.py
@@ -17,13 +17,13 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(upload_to='product_images/', default='test_picture.jpg')
     category = models.CharField(max_length=100,
                                 default='1')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    manufactured_at = models.DateTimeField()
+    manufactured_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
