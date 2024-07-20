13cd04f321d9d58c41d91803f88b2ee19659d3b3
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-07-20 15:26:03 +0700

    Some fix

--- catalog/views.py
+++ catalog/views.py
@@ -10,7 +10,6 @@ def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
 
-# Product views
 class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
@@ -29,26 +28,25 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_detail.html'
-
-class VersionCreateView(CreateView):
-    model = Version
-    form_class = VersionForm
-    template_name = 'version_form.html'
+#
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product/product_form.html'
     success_url = reverse_lazy('product_list')
 
     def form_valid(self, form):
-        product_id = self.kwargs['product_id']
-        form.instance.product = get_object_or_404(Product, id=product_id)
-        response = super().form_valid(form)
-        if form.instance.is_current:
-            Version.objects.filter(product=form.instance.product).exclude(id=form.instance.id).update(is_current=False)
-        messages.success(self.request, 'Version was successfully created.')
-        return response
+        messages.success(self.request, 'Product was successfully created.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'There was an error creating the product. Please check the form for errors.')
+        return super().form_invalid(form)
 
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
-    template_name = 'product_form.html'
+    template_name = 'product/product_form.html'
     success_url = reverse_lazy('product_list')
 
     def form_valid(self, form):
@@ -68,7 +66,6 @@ class ProductDeleteView(DeleteView):
         messages.success(request, 'Product was successfully deleted.')
         return super().delete(request, *args, **kwargs)
 
-# Version views
 class VersionCreateView(CreateView):
     model = Version
     form_class = VersionForm
