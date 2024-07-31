8b8a8c5fcee0c3dd7ce4240d0ba8d611bb8f9894
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-07-31 08:13:57 +0700

    Some fix

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,7 @@ from .views import (
     CombinedPasswordResetView,
     CustomLoginView,
     VerifyEmailView,
+    LoginView,
 )
 
 urlpatterns = [
@@ -15,7 +16,7 @@ urlpatterns = [
     path("register/", RegistrationView.as_view(), name="register"),
     path("profile/", UserProfileView.as_view(), name="user_profile"),
     path("profile/edit/", UserUpdateView.as_view(), name="user_update"),
-    path("login/", CustomLoginView.as_view(), name="login"),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(
         "password_change/", CustomPasswordChangeView.as_view(), name="password_change"
